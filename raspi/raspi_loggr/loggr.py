267df1129b2ea04bd1040463848ff3ee060c6ab7
Iniesta8 <andi.schnebinger@googlemail.com>
2015-04-25 13:13:27 +0200

    feat(raspi): implement send data to firebase

--- raspi/raspi_loggr/loggr.py
+++ raspi/raspi_loggr/loggr.py
@@ -2,11 +2,27 @@
 # -*- coding: utf-8 -*-
 
 from .sensor import Sensor
+import requests
+import json
+import time
 
 emu_sensor = Sensor('emu_sensor')
 
+FIREBASE = 'https://loggrio.firebaseio.com'
+
 def main():
-  print("out: " + emu_sensor.get_measure())
+  curTime = str(time.time())
+  value = emu_sensor.get_measure()
+  sensor_type = 'emu_temp'
+  payload = {'type' : sensor_type, 'time' : curTime, 'value' : value}
+
+  print sendData(payload)
+
+
+def sendData(payload):
+  r = requests.post(FIREBASE + "/" + payload.get('type') + ".json", data=json.dumps(payload))
+  return r.status_code
+
 
 if __name__ == "__main__":
     main()
