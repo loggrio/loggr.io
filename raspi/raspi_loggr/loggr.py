3aaf2348880342322d2998c8281184010ccdad92
Felix Itzenplitz <felix@stkn.org>
2015-04-28 18:05:26 +0200

    fix(raspi): format code

--- raspi/raspi_loggr/loggr.py
+++ raspi/raspi_loggr/loggr.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 from .sensor import Sensor
 from .util import sendData
 
-from datetime import datetime
 
 emu_sensor = Sensor('emu_sensor')
 
-
 def main():
-  curTime = str(datetime.now())
+  now = str(datetime.now())
   value = emu_sensor.get_metering()
   sensor_type = 'emu_temp'
 
   payload = {'userId': '1',
-          'sensorType' : sensor_type,
-          'time' : curTime,
-          'value' : value}
+              'sensorType' : sensor_type,
+              'time' : now,
+              'value' : value}
 
   print sendData(payload)
-
-
-if __name__ == "__main__":
-    main()
--- raspi/run.py
+++ raspi/run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from raspi_loggr.loggr import main
 
 if __name__ == "__main__":
