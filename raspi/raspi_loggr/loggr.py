e50d0fa02d6cd0c565c6a606794c3b9c89caba53
Felix Itzenplitz <felix@stkn.org>
2015-05-24 11:04:38 +0200

    feat(raspi): code cleanup

--- raspi/raspi_loggr/loggr.py
+++ raspi/raspi_loggr/loggr.py
@@ -3,11 +3,8 @@
 import time
 import logging
 from enum import Enum
-from datetime import datetime
 
 from .sensor import Sensor
-from .util import send_data
-from .util import gen_payload
 from .util import set_status_led
 
 
@@ -27,9 +24,9 @@ class ValueUnits(Enum):
 
 
 # emu_sensor = Sensor('emu_sensor')
-temperature = Sensor('0', 'tempSensor1', 'exampleRoom', SensorTypes.temperature.name)
-brightness = Sensor('1', 'brightSensor1', 'exampleRoom', SensorTypes.brightness.name)
-humidity = Sensor('2', 'humidSensor1', 'exampleRoom', SensorTypes.humidity.name)
+temperature = Sensor('tempSensor1', 'exampleRoom', SensorTypes.temperature.name, ValueUnits.grad_celsius.name)
+brightness = Sensor('brightSensor1', 'exampleRoom', SensorTypes.brightness.name, ValueUnits.lumen.name)
+humidity = Sensor('humidSensor1', 'exampleRoom', SensorTypes.humidity.name, ValueUnits.percent.name)
 # volume = Sensor('3', 'volSensor1', 'exampleRoom', SensorTypes.volume.name)
 
 TIME_BETWEEN_METERINGS = 60
@@ -39,36 +36,10 @@ def main():
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', filename='loggr.log', level=logging.INFO)
     logging.info('Logging (re)started')
 
-    # TODO: uniqe userId management
-    userId = '1'
-
     while (True):
-        # get metering from temperature sensor
-        value_temp = temperature.get_metering()
-        now = str(datetime.now())
-        payload_temp = gen_payload(userId, temperature.sensor_type, temperature.sensor_name, now, value_temp,
-                                   ValueUnits.grad_celsius.name)
-
-        # get metering from brightness sensor
-        value_bright = brightness.get_metering()
-        now = str(datetime.now())
-        payload_bright = gen_payload(userId, brightness.sensor_type, brightness.sensor_name, now, value_bright,
-                                     ValueUnits.lumen.name)
-
-        # get metering from humidity sensor
-        value_humid = humidity.get_metering()
-        now = str(datetime.now())
-        payload_humid = gen_payload(userId, humidity.sensor_type, humidity.sensor_name, now, value_humid,
-                                    ValueUnits.percent.name)
-
-        # get metering from volume sensor
-        # value_vol = volume.get_metering()
-        # payload_vol = gen_payload(userId, volume.sensor_type, volume.sensor_name, now, value_vol,
-        #                           ValueUnits.decibel.name)
-
-        ret1 = send_data(payload_temp)
-        ret2 = send_data(payload_bright)
-        ret3 = send_data(payload_humid)
+        ret1 = temperature.meter_and_send()
+        ret2 = brightness.meter_and_send()
+        ret3 = humidity.meter_and_send()
         # ret4 = send_data(payload_vol)
 
         if ret1 == 200 and ret2 == 200 and ret3 == 200:
--- raspi/raspi_loggr/util.py
+++ raspi/raspi_loggr/util.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests
-import json
 import subprocess
-import logging
-
-LOG_FILE = 'log.txt'
-API = 'http://stkn.org:3001/api'
-DB = 'meterings'
-
-
-def send_data(payload):
-    headers = {'Content-Type': 'application/json'}
-    r = requests.post(API + "/" + DB, data=json.dumps(payload), headers=headers)
-    return r.status_code
-
-
-def gen_payload(user_id, sensor_type, sensor_name, time, value, unit):
-    payload = {'userId': user_id,
-               'sensorType': sensor_type,
-               'sensorName': sensor_name,
-               'time': time,
-               'value': value,
-               'unit': unit}
-    return payload
 
 
 def set_status_led(status_code):
