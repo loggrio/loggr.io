e88279368ab7a03f0f3fe825e88d679337a6ff08
Felix Itzenplitz <felix@stkn.org>
2015-05-28 20:08:03 +0200

    feat(h5vzr): add icons to repo

--- h5vzr/app/scripts/app.js
+++ h5vzr/app/scripts/app.js
@@ -33,16 +33,16 @@ angular
       .primaryPalette('teal');
 
     $mdIconProvider
-      .icon('account-circle', 'bower_components/material-design-icons/action/svg/production/ic_account_circle_24px.svg', 24)
-      .icon('options', 'bower_components/material-design-icons/navigation/svg/production/ic_menu_24px.svg', 24)
-      .icon('add', 'bower_components/material-design-icons/content/svg/production/ic_add_24px.svg', 24)
-      .icon('edit', 'bower_components/material-design-icons/image/svg/production/ic_edit_24px.svg', 24)
-      .icon('tune', 'bower_components/material-design-icons/image/svg/production/ic_tune_24px.svg', 24)
-      .icon('more-vert', 'bower_components/material-design-icons/navigation/svg/production/ic_more_vert_24px.svg', 24)
-      .icon('email', 'bower_components/material-design-icons/communication/svg/production/ic_email_24px.svg', 24)
-      .icon('location', 'bower_components/material-design-icons/communication/svg/production/ic_location_on_24px.svg', 24)
-      .icon('password', 'bower_components/material-design-icons/communication/svg/production/ic_security_24px.svg', 24)
-      .icon('loggr-icon', 'images/loggr_icon_white.svg', 48);
+      .icon('account-circle', 'images/svg/ic_account_circle_24px.svg', 24)
+      .icon('options', 'images/svg/ic_menu_24px.svg', 24)
+      .icon('add', 'images/svg/ic_add_24px.svg', 24)
+      .icon('edit', 'images/svg/ic_edit_24px.svg', 24)
+      .icon('tune', 'images/svg/ic_tune_24px.svg', 24)
+      .icon('more-vert', 'images/svg/ic_more_vert_24px.svg', 24)
+      .icon('email', 'images/svg/ic_email_24px.svg', 24)
+      .icon('location', 'images/svg/ic_location_on_24px.svg', 24)
+      .icon('password', 'images/svg/ic_security_24px.svg', 24)
+      .icon('loggr-icon', 'images/svg/loggr_icon_white.svg', 48);
 
     // TODO: seperate cfg file
     LoopBackResourceProvider.setUrlBase('http://localhost:3000/api/');
