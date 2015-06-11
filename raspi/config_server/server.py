4d6596c65c7fa51a4dcf32650fd1ef9cb6db2a46
Andi Schnebinger <andi.schnebinger@googlemail.com>
2015-06-11 19:24:46 +0200

    feat(raspi:config_server): write token to file

--- raspi/config_server/server.py
+++ raspi/config_server/server.py
@@ -1,9 +1,20 @@
 from flask import Flask, request, jsonify
+from os import path
+from ConfigParser import ConfigParser
 server = Flask(__name__)
+config = ConfigParser()
+
+HOME_DIR = os.getenv("HOME")
+CONFIG_FILE = HOME_DIR + '/.loggrrc'
+
 
 @server.route('/', methods=['POST'])
-def get_token():
-    print request.json
+def save_token():
+    # TODO: config file exists?
+    config.read(CONFIG_FILE)
+    config.set('AUTH', 'token', request.json['token'])
+    with open(CONFIG_FILE, 'w') as configfile:
+        config.write(configfile)
     return jsonify(status='ok')
 
 if __name__ == "__main__":
