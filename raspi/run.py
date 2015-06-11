a53d7af2a42c6426a71fb9366db3f246f188936a
Andi Schnebinger <andi.schnebinger@googlemail.com>
2015-06-11 20:16:52 +0200

    fix(raspi:config_server): fix imports and run script

--- raspi/run.py
+++ raspi/run.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
-import subprocess
-from raspi_loggr.util import init_config
 
 if __name__ == "__main__":
-    init_config()
     if len(sys.argv) > 1 and sys.argv[1] == "--emu":
         from raspi_loggr.emu import main as emu_main
         emu_main()
