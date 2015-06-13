7e954fc20c85f1c3df048e75e3da72b7ed91ba2b
Felix Itzenplitz <felix@stkn.org>
2015-06-13 11:31:24 +0200

    feat(h5vzr:login): add http interceptor

--- h5vzr/app/scripts/app.js
+++ h5vzr/app/scripts/app.js
@@ -18,7 +18,8 @@ angular
     'lbServices',
     'ng-sortable'
   ])
-  .config(function ($routeProvider, $mdThemingProvider, $mdIconProvider, LoopBackResourceProvider) {
+  .config(function ($routeProvider, $httpProvider, $mdThemingProvider, $mdIconProvider, LoopBackResourceProvider) {
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -39,4 +40,20 @@ angular
 
     // TODO: seperate cfg file
     LoopBackResourceProvider.setUrlBase('http://localhost:3000/api/');
+
+    $httpProvider.interceptors.push(function($q, $location, LoopBackAuth) {
+      return {
+        responseError: function(rejection) {
+          if (rejection.status === 401) {
+            //Now clearing the loopback values from client browser for safe logout...
+            LoopBackAuth.clearUser();
+            LoopBackAuth.clearStorage();
+            $location.nextAfterLogin = $location.path();
+            $location.path('/login');
+          }
+          return $q.reject(rejection);
+        }
+      };
+    });
+
   });
