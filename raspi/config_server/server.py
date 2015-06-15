165027f26410bd800fc9b8b67340eee0fc56169d
Jessica Zimpel <jessica-zimpel@gmx.de>
2015-06-15 20:10:13 +0200

    feat(raspi): add regex and availability test

--- raspi/config_server/server.py
+++ raspi/config_server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from os import path
+import re
 from ConfigParser import ConfigParser
 server = Flask(__name__)
 config = ConfigParser()
@@ -11,10 +12,23 @@ CONFIG_FILE = HOME_DIR + '/.loggrrc'
 @server.route('/', methods=['POST'])
 def save_token_and_userid():
     config.read(CONFIG_FILE)
-    # TODO: regex token
-    # TODO: regex userid: 24 char
-    config.set('AUTH', 'token', request.json['token'])
-    config.set('AUTH', 'userid', request.json['userid'])
+
+    regex_userid = re.compile(r'^[a-z,0-9]{24}$')
+    regex_token = re.compile(r'^[A-Z,a-z,0-9]{64}$')
+
+    if 'token' not in request.json and 'userid' not in request.json:
+        return jsonify(error='arguments')
+
+    if 'token' in request.json:
+        if regex_token.match(request.json['token']) is None:
+            return jsonify(error='format')
+        config.set('AUTH', 'token', request.json['token'])
+
+    if 'userid' in request.json:
+        if regex_userid.match(request.json['userid']) is None:
+            return jsonify(error='format')
+        config.set('AUTH', 'userid', request.json['userid'])
+
     with open(CONFIG_FILE, 'w') as configfile:
         config.write(configfile)
     return jsonify(status='ok')
@@ -31,4 +45,4 @@ def init_config():
 
 if __name__ == "__main__":
     init_config()
-    server.run(host='0.0.0.0')
+    server.run(host='0.0.0.0', debug=True)
