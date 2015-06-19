eaf44cf729d441e45ecac9d765f01bddaf795ce1
Andi Schnebinger <andi.schnebinger@googlemail.com>
2015-06-19 14:39:18 +0200

    feat(config_server): add logging functionality in config server

--- raspi/config_server/server.py
+++ raspi/config_server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from os import path
 import re
+import logging
 from ConfigParser import ConfigParser
 from flask.ext.cors import CORS
 server = Flask(__name__)
@@ -20,17 +21,27 @@ def save_token_and_userid():
     regex_token = re.compile(r'^[A-Z,a-z,0-9]{64}$')
 
     if 'token' not in request.json and 'userid' not in request.json:
+        logging.error('missing arguments in post request')
+        print 'missing arguments in post request'
         return jsonify(error='arguments')
 
     if 'token' in request.json:
         if regex_token.match(request.json['token']) is None:
+            logging.error('wrong format of token string')
+            print 'wrong format of token string'
             return jsonify(error='format')
         config.set('AUTH', 'token', request.json['token'])
+        logging.info('token set in config file')
+        print 'token set in config file'
 
     if 'userid' in request.json:
         if regex_userid.match(request.json['userid']) is None:
+            logging.error('wrong format of userid string')
+            print 'wrong format of userid string'
             return jsonify(error='format')
         config.set('AUTH', 'userid', request.json['userid'])
+        logging.info('userid set in config file')
+        print 'userid set in config file'
 
     with open(CONFIG_FILE, 'w') as configfile:
         config.write(configfile)
@@ -40,12 +51,17 @@ def save_token_and_userid():
 def init_config():
     # If config file not exists create a default one
     if not path.isfile(CONFIG_FILE):
+        logging.info('No config file found, create default one')
+        print 'No config file found, craete default one'
         config.add_section('AUTH')
         config.add_section('SENSORS')
         config.set('AUTH', 'token', '')
         config.set('AUTH', 'userid', '')
         with open(CONFIG_FILE, 'w') as configfile:
             config.write(configfile)
+        logging.info('Created default config file')
+        print 'Created default config file'
+        return
 
     config.read(CONFIG_FILE)
 
@@ -64,6 +80,8 @@ def init_config():
     with open(CONFIG_FILE, 'w') as configfile:
         config.write(configfile)
 
+
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s', filename='server.log', level=logging.INFO)
     init_config()
     server.run(host='0.0.0.0', debug=True)
