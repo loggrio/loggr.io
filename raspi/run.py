31be79b82ac919a274178cbf0a5714ffd7aceac9
Andi Schnebinger <andi.schnebinger@googlemail.com>
2015-06-21 20:07:50 +0200

    feat(raspi): one run script for everything

--- raspi/run.py
+++ raspi/run.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--emu":
         from raspi_loggr.emu import main as emu_main
         emu_main()
+    elif len(sys.argv) > 1 and sys.argv[1] == "--config_server":
+        from config_server.server import main as server_main
+        server_main()
     else:
         from raspi_loggr.loggr import main
         main()
