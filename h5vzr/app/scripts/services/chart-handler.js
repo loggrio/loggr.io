9e190ae9cc79aefa9c76aa2f72310e5bc5d8745e
Felix Itzenplitz <felix@stkn.org>
2015-06-24 19:24:46 +0200

    fix(h5vzr): spaces spaces

--- h5vzr/app/scripts/services/chart-handler.js
+++ h5vzr/app/scripts/services/chart-handler.js
@@ -17,7 +17,7 @@ angular.module('loggrioApp')
   this.sensorsInUse = [];
   this.promises = [];
 
-  /* deletes items of the arrays and stops all running intervals */
+  // deletes items of the arrays and stops all running intervals
   this.initialize = function() {
     self.customerId = Customer.getCurrentId();
     self.chartConfig.splice(0,self.chartConfig.length);
@@ -30,24 +30,24 @@ angular.module('loggrioApp')
 
   this.goLive = function() {
     self.initialize();
-    //get all sensors from the current customer
+    // get all sensors from the current customer
     Customer.sensors({id: self.customerId}).$promise.then(function (sensors) {
-      //generate list of all available sensors for the sortable list
+      // generate list of all available sensors for the sortable list
       angular.forEach(sensors, function(sensor, index){
         self.sensors.push(sensor);
-        //check if sensor is in use in put in the right order
+        // check if sensor is in use in put in the right order
         var position = util.sensorIsInUse(sensor);
-        if(position > -1){ //in sensorInUse list
+        if(position > -1){ // in sensorInUse list
           self.sensorsInUse[position] = sensor;
-        } else if (position === -2){ //viewConfig non existing
+        } else if (position === -2){ // viewConfig non existing
           self.sensorsInUse[index] = sensor;
         }
       });
 
-      //go through all sensors in use to generate acording charts
+      // go through all sensors in use to generate acording charts
       angular.forEach(self.sensorsInUse, function(sensor, index) {
         self.chartConfig[index] = new ChartConfig(sensor);
-        //get metering to acording sensor
+        // get metering to acording sensor
         Customer.meterings({id: self.customerId, filter: {where: {sensorId: sensor.id}}})
           .$promise.then(function (meterings) {
 
@@ -57,7 +57,7 @@ angular.module('loggrioApp')
 
             var lastTime = meterings.length ? meterings[meterings.length - 1].time : 0;
             var shift;
-            //live reload
+            // live reload
             self.promises[sensor.id] = $interval(function () {
               // shift on more than 5 dots
               shift = chart.series[0].data.length > 5;
