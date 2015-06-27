b089abdefd7d45474532e794e4f9c757d45f13d9
Felix Itzenplitz <felix@stkn.org>
2015-06-27 12:11:10 +0200

    style(h5vzr): reformat

--- h5vzr/app/scripts/services/chart-config.js
+++ h5vzr/app/scripts/services/chart-config.js
@@ -1,132 +1,132 @@
 'use strict';
 
 /**
-* @ngdoc service
-* @name loggrioApp.chartConfig
-* @description
-* # ChartConfig
-* Service in the loggrioApp.
-*/
+ * @ngdoc service
+ * @name loggrioApp.chartConfig
+ * @description
+ * # ChartConfig
+ * Service in the loggrioApp.
+ */
 angular.module('loggrioApp')
-.service('chartConfig', function () {
-  var yAxisText;
-  var tooltipSuffixText;
-  var titleText;
-  var seriesName;
-  var configStub = {};
-  configStub = {
-    id: '',
-    options: {
-      lang: {
-        contextButtonTitle: 'Graphenoptionen',
-        downloadJPEG: 'Graph als JPEG exportieren',
-        downloadPDF: 'Graph als PDF exportieren',
-        downloadPNG: 'Graph als PNG exportieren',
-        downloadSVG: 'Graph als SVG exportieren',
-        loading: 'Daten werden geladen...',
-        months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-        noData: 'Keine Daten zum Anzeigen vorhanden',
-        printChart: 'Graph drucken',
-        shortMonths: ['Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
-        weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
-      },
-      title: {
-        text: ''
-      },
-      chart: '',
-      xAxis: '',
-      yAxis: {
+  .service('chartConfig', function () {
+    var yAxisText;
+    var tooltipSuffixText;
+    var titleText;
+    var seriesName;
+    var configStub = {};
+    configStub = {
+      id: '',
+      options: {
+        lang: {
+          contextButtonTitle: 'Graphenoptionen',
+          downloadJPEG: 'Graph als JPEG exportieren',
+          downloadPDF: 'Graph als PDF exportieren',
+          downloadPNG: 'Graph als PNG exportieren',
+          downloadSVG: 'Graph als SVG exportieren',
+          loading: 'Daten werden geladen...',
+          months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+          noData: 'Keine Daten zum Anzeigen vorhanden',
+          printChart: 'Graph drucken',
+          shortMonths: ['Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+          weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
+        },
         title: {
           text: ''
+        },
+        chart: '',
+        xAxis: '',
+        yAxis: {
+          title: {
+            text: ''
+          }
+        },
+        tooltip: {
+          valueSuffix: ''
         }
       },
-      tooltip: {
-        valueSuffix: ''
-      }
-    },
-    series: [{
-      data: [],
-      color: '#009688',
-      name: ''
-    }]
-  };
+      series: [{
+        data: [],
+        color: '#009688',
+        name: ''
+      }]
+    };
 
-  var setChartTexts = function (sensor) {
-    switch (sensor.type) {
-      case 'temperature':
-        titleText = 'Temperatur' + ' ' + sensor.location;
-        yAxisText = 'Temperatur (°C)';
-        tooltipSuffixText = ' °C';
-        seriesName = 'Temperatur';
+    var setChartTexts = function (sensor) {
+      switch (sensor.type) {
+        case 'temperature':
+          titleText = 'Temperatur' + ' ' + sensor.location;
+          yAxisText = 'Temperatur (°C)';
+          tooltipSuffixText = ' °C';
+          seriesName = 'Temperatur';
+          break;
+        case 'pressure':
+          titleText = 'Luftdruck' + ' ' + sensor.location;
+          yAxisText = 'Luftdruck (hPa)';
+          tooltipSuffixText = ' hPa';
+          seriesName = 'Luftdruck';
         break;
-      case 'pressure':
-        titleText = 'Luftdruck' + ' ' + sensor.location;
-        yAxisText = 'Luftdruck (hPa)';
-        tooltipSuffixText = ' hPa';
-        seriesName = 'Luftdruck';
-      break;
-      case 'brightness':
-        titleText = 'Helligkeit' + ' ' + sensor.location;
-        yAxisText = 'Helligkeit (lm)';
-        tooltipSuffixText = ' lm';
-        seriesName = 'Helligkeit';
-      break;
-      case 'humidity':
-        titleText = 'Luftfeuchtigkeit' + ' ' + sensor.location;
-        yAxisText = 'Relative Luftfeuchtigkeit (%)';
-        tooltipSuffixText = ' %';
-        seriesName = 'Luftfeuchtigkeit';
-      break;
-      default:
-        titleText = 'Error: Unknown sensor type';
-        yAxisText = '';
-        tooltipSuffixText = '';
-        seriesName = '';
-    }
-  };
-
-  this.getSplineChartConfig = function (sensor) {
-    setChartTexts(sensor);
-
-    var chartConfig = angular.copy(configStub);
-    chartConfig.id = sensor.id;
-    chartConfig.options.title.text = titleText;
-    chartConfig.options.chart = {
-      zoomType: 'x',
-      type: 'spline',
-      resetZoomButton: {
-        theme: {
-          display: 'none'
-        }
+        case 'brightness':
+          titleText = 'Helligkeit' + ' ' + sensor.location;
+          yAxisText = 'Helligkeit (lm)';
+          tooltipSuffixText = ' lm';
+          seriesName = 'Helligkeit';
+        break;
+        case 'humidity':
+          titleText = 'Luftfeuchtigkeit' + ' ' + sensor.location;
+          yAxisText = 'Relative Luftfeuchtigkeit (%)';
+          tooltipSuffixText = ' %';
+          seriesName = 'Luftfeuchtigkeit';
+        break;
+        default:
+          titleText = 'Error: Unknown sensor type';
+          yAxisText = '';
+          tooltipSuffixText = '';
+          seriesName = '';
       }
     };
-    chartConfig.options.xAxis = {
-      type: 'datetime'
+
+    this.getSplineChartConfig = function (sensor) {
+      setChartTexts(sensor);
+
+      var chartConfig = angular.copy(configStub);
+      chartConfig.id = sensor.id;
+      chartConfig.options.title.text = titleText;
+      chartConfig.options.chart = {
+        zoomType: 'x',
+        type: 'spline',
+        resetZoomButton: {
+          theme: {
+            display: 'none'
+          }
+        }
+      };
+      chartConfig.options.xAxis = {
+        type: 'datetime'
+      };
+      chartConfig.options.yAxis.title.text = yAxisText;
+      chartConfig.options.tooltip.valueSuffix = tooltipSuffixText;
+      chartConfig.series[0].name = seriesName;
+
+      return chartConfig;
     };
-    chartConfig.options.yAxis.title.text = yAxisText;
-    chartConfig.options.tooltip.valueSuffix = tooltipSuffixText;
-    chartConfig.series[0].name = seriesName;
 
-    return chartConfig;
-  };
+    this.getColumnChartConfig = function (sensor) {
+      setChartTexts(sensor);
 
-  this.getColumnChartConfig = function (sensor) {
-    setChartTexts(sensor);
+      var chartConfig = angular.copy(configStub);
+      chartConfig.id = sensor.id;
+      chartConfig.options.title.text = titleText;
+      chartConfig.options.chart = {
+        type: 'column'
+      };
+      chartConfig.options.xAxis = {
+        categories: configStub.options.lang.weekdays
+      };
+      chartConfig.options.yAxis.title.text = yAxisText;
+      chartConfig.options.tooltip.valueSuffix = tooltipSuffixText;
+      chartConfig.series[0].name = seriesName + ' im Durchschnitt';
 
-    var chartConfig = angular.copy(configStub);
-    chartConfig.id = sensor.id;
-    chartConfig.options.title.text = titleText;
-    chartConfig.options.chart = {
-      type: 'column'
+      return chartConfig;
+
     };
-    chartConfig.options.xAxis = {
-      categories: configStub.options.lang.weekdays
-    };
-    chartConfig.options.yAxis.title.text = yAxisText;
-    chartConfig.options.tooltip.valueSuffix = tooltipSuffixText;
-    chartConfig.series[0].name = seriesName + ' im Durchschnitt';
-
-    return chartConfig;
-
-  };
-});
+  });
--- h5vzr/app/scripts/services/chart-handler.js
+++ h5vzr/app/scripts/services/chart-handler.js
@@ -1,22 +1,22 @@
 'use strict';
 
 /**
-* @ngdoc service
-* @name loggrioApp.chartHandler
-* @description
-* # chartHandler
-* Service in the loggrioApp.
-*/
+ * @ngdoc service
+ * @name loggrioApp.chartHandler
+ * @description
+ * # chartHandler
+ * Service in the loggrioApp.
+ */
 angular.module('loggrioApp')
-.service('chartHandler', function ($interval, $timeout, Customer, Metering, notify, util, zoom, chartConfig) {
-  var self = this;
+  .service('chartHandler', function ($interval, $timeout, Customer, Metering, notify, util, zoom, chartConfig) {
+    var self = this;
 
-  this.charts = [];
-  this.chartConfigs = [];
-  this.averageChartConfigs = [];
-  this.sensors = [];
-  this.sensorsInUse = [];
-  this.promises = [];
+    this.charts = [];
+    this.chartConfigs = [];
+    this.averageChartConfigs = [];
+    this.sensors = [];
+    this.sensorsInUse = [];
+    this.promises = [];
 
   // deletes items of the arrays and stops all running intervals
   this.initialize = function() {
@@ -35,65 +35,65 @@ angular.module('loggrioApp')
     }
   };
 
-  this.goLive = function() {
-    self.initialize();
-    // get all sensors from the current customer
-    return Customer.sensors({id: self.customerId}).$promise.then(function (sensors) {
-      // generate list of all available sensors for the sortable list
-      angular.forEach(sensors, function(sensor, index){
-        self.sensors.push(sensor);
-        // check if sensor is in use in put in the right order
-        var position = util.sensorIsInUse(sensor);
-        if(position > -1){ // in sensorInUse list
-          self.sensorsInUse[position] = sensor;
-        } else if (position === -2){ // viewConfig non existing
-          self.sensorsInUse[index] = sensor;
-        }
-      });
-
-      // go through all sensors in use to generate acording charts
-      angular.forEach(self.sensorsInUse, function(sensor, index) {
-        self.charts[index] = {
-          default: chartConfig.getSplineChartConfig(sensor),
-          average: chartConfig.getColumnChartConfig(sensor),
-          viewToggled: false,
-          viewToggle: function () {
-            this.viewToggled = !this.viewToggled;
+    this.goLive = function() {
+      self.initialize();
+      // get all sensors from the current customer
+      return Customer.sensors({id: self.customerId}).$promise.then(function (sensors) {
+        // generate list of all available sensors for the sortable list
+        angular.forEach(sensors, function(sensor, index){
+          self.sensors.push(sensor);
+          // check if sensor is in use in put in the right order
+          var position = util.sensorIsInUse(sensor);
+          if(position > -1){ // in sensorInUse list
+            self.sensorsInUse[position] = sensor;
+          } else if (position === -2){ // viewConfig non existing
+            self.sensorsInUse[index] = sensor;
           }
-        };
-        // get metering to acording sensor
-        Customer.meterings({id: self.customerId, filter: {where: {sensorId: sensor.id}}})
-          .$promise.then(function (meterings) {
-            var chart = self.charts[index].default.getHighcharts();
-            var averageChart = self.charts[index].average.getHighcharts();
-            var data = util.meteringToChartData(meterings).default;
-            var averageData = util.meteringToChartData(meterings).averageWeek;
-            chart.series[0].setData(data, true);
-            averageChart.series[0].setData(averageData, true);
+        });
 
-            var lastTime = meterings.length ? meterings[meterings.length - 1].time : 0;
-            var shift;
-            // live reload
-            self.promises[sensor.id] = $interval(function () {
-              // shift on more than 5 dots
-              shift = chart.series[0].data.length > 5;
-              Customer.meterings({id: self.customerId, filter: {where: {time: {gt: lastTime}, sensorId: sensor.id}}})
-                .$promise.then(function (meterings) {
-                  if (meterings.length) {
-                    lastTime = meterings[meterings.length - 1].time;
+        // go through all sensors in use to generate acording charts
+        angular.forEach(self.sensorsInUse, function(sensor, index) {
+          self.charts[index] = {
+            default: chartConfig.getSplineChartConfig(sensor),
+            average: chartConfig.getColumnChartConfig(sensor),
+            viewToggled: false,
+            viewToggle: function () {
+              this.viewToggled = !this.viewToggled;
+            }
+          };
+          // get metering to acording sensor
+          Customer.meterings({id: self.customerId, filter: {where: {sensorId: sensor.id}}})
+            .$promise.then(function (meterings) {
+              var chart = self.charts[index].default.getHighcharts();
+              var averageChart = self.charts[index].average.getHighcharts();
+              var data = util.meteringToChartData(meterings).default;
+              var averageData = util.meteringToChartData(meterings).averageWeek;
+              chart.series[0].setData(data, true);
+              averageChart.series[0].setData(averageData, true);
 
-                    var data = util.meteringToChartData(meterings).default;
-                    angular.forEach(data, function (value) {
-                      if(shift){
-                        zoom.shift(chart, value[0]);
-                      }
-                      $timeout(chart.series[0].addPoint(value, true, false), 1000);
-                    });
-                  }
-                });
-            }, 10000);
-          });
+              var lastTime = meterings.length ? meterings[meterings.length - 1].time : 0;
+              var shift;
+              // live reload
+              self.promises[sensor.id] = $interval(function () {
+                // shift on more than 5 dots
+                shift = chart.series[0].data.length > 5;
+                Customer.meterings({id: self.customerId, filter: {where: {time: {gt: lastTime}, sensorId: sensor.id}}})
+                  .$promise.then(function (meterings) {
+                    if (meterings.length) {
+                      lastTime = meterings[meterings.length - 1].time;
+
+                      var data = util.meteringToChartData(meterings).default;
+                      angular.forEach(data, function (value) {
+                        if(shift){
+                          zoom.shift(chart, value[0]);
+                        }
+                        $timeout(chart.series[0].addPoint(value, true, false), 1000);
+                      });
+                    }
+                  });
+              }, 10000);
+            });
+        });
       });
-    });
-  };
-});
+    };
+  });
