c2a5466e65f6a8c6936821aaef07d78efede3c64
SimoKutlin <simo.kutlin@gmail.com>
2015-06-29 12:13:38 +0200

    fix(h5vzr:chartConfig): fix unit handling

--- h5vzr/app/scripts/services/chart-config.js
+++ h5vzr/app/scripts/services/chart-config.js
@@ -50,30 +50,47 @@ angular.module('loggrioApp')
       }]
     };
 
+    var setUnit = function (sensorUnit) {
+      switch (sensorUnit) {
+        case 'grad_celsius':
+          return '°C';
+        case 'lumen':
+          return 'lm';
+        case 'percent':
+          return '%';
+        case 'hectopascal':
+          return 'hPa';
+        default:
+          return '';
+      }
+    };
+
     var setChartTexts = function (sensor) {
+      var unitText = setUnit(sensor.unit);
+
       switch (sensor.type) {
         case 'temperature':
           titleText = 'Temperatur' + ' ' + sensor.location;
-          yAxisText = 'Temperatur (' + sensor.unit + ')';
-          tooltipSuffixText = ' °C';
+          yAxisText = 'Temperatur (' + unitText + ')';
+          tooltipSuffixText = ' ' + unitText;
           seriesName = 'Temperatur';
           break;
         case 'pressure':
           titleText = 'Luftdruck' + ' ' + sensor.location;
-          yAxisText = 'Luftdruck (' + sensor.unit + ')';
-          tooltipSuffixText = ' ' + sensor.unit;
+          yAxisText = 'Luftdruck (' + unitText + ')';
+          tooltipSuffixText = ' ' + unitText;
           seriesName = 'Luftdruck';
         break;
         case 'brightness':
           titleText = 'Helligkeit' + ' ' + sensor.location;
-          yAxisText = 'Helligkeit (' + sensor.unit + ')';
-          tooltipSuffixText = ' ' + sensor.unit;
+          yAxisText = 'Helligkeit (' + unitText + ')';
+          tooltipSuffixText = ' ' + unitText;
           seriesName = 'Helligkeit';
         break;
         case 'humidity':
           titleText = 'Luftfeuchtigkeit' + ' ' + sensor.location;
-          yAxisText = 'Relative Luftfeuchtigkeit (' + sensor.unit + ')';
-          tooltipSuffixText = ' ' + sensor.unit;
+          yAxisText = 'Relative Luftfeuchtigkeit (' + unitText + ')';
+          tooltipSuffixText = ' ' + unitText;
           seriesName = 'Luftfeuchtigkeit';
         break;
         default:
