5ee813898776bdab413cb3f9fd4302873e1d1de2
Marcel <marcelpuetz@web.de>
2015-06-29 14:01:41 +0200

    fix(h5vzr): timzone of highcharts

--- h5vzr/app/scripts/services/chart-config.js
+++ h5vzr/app/scripts/services/chart-config.js
@@ -16,19 +16,6 @@ angular.module('loggrioApp')
     var configStub = {
       id: '',
       options: {
-        lang: {
-          contextButtonTitle: 'Graphenoptionen',
-          downloadJPEG: 'Graph als JPEG exportieren',
-          downloadPDF: 'Graph als PDF exportieren',
-          downloadPNG: 'Graph als PNG exportieren',
-          downloadSVG: 'Graph als SVG exportieren',
-          loading: 'Daten werden geladen...',
-          months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
-          noData: 'Keine Daten zum Anzeigen vorhanden',
-          printChart: 'Graph drucken',
-          shortMonths: ['Jan', 'Feb', 'Mrz', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
-          weekdays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
-        },
         title: {
           text: ''
         },
