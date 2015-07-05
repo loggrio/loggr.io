a41152e67596e695493160625c156e9898fd729b
Felix Itzenplitz <felix@stkn.org>
2015-07-05 13:09:28 +0200

    fix(h5vzr): fix average

--- h5vzr/app/scripts/services/util.js
+++ h5vzr/app/scripts/services/util.js
@@ -12,23 +12,27 @@ angular.module('loggrioApp')
 
     function getWeekDay(weekDay) {
       switch (weekDay) {
-          case '0':
+          case 0:
             return 'Sonntag';
-          case '1':
+          case 1:
             return 'Montag';
-          case '2':
+          case 2:
             return 'Dienstag';
-          case '3':
+          case 3:
             return 'Mittwoch';
-          case '4':
+          case 4:
             return 'Donnerstag';
-          case '5':
+          case 5:
             return 'Freitag';
-          case '6':
+          case 6:
             return 'Samstag';
           default:
             return 'Falscher Tag';
       }
+    }
+
+    function round(value) {
+      return Math.round(value * 100) / 100;
     }
 
     this.meteringToChartData = function (meterings) {
@@ -40,9 +44,10 @@ angular.module('loggrioApp')
         }
       };
 
-      var days = [];
       var oneWeekAgo = new Date();
       oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      oneWeekAgo = oneWeekAgo.getTime();
+      var counter = 0;
 
       angular.forEach(meterings, function(metering) {
         var date = Date.parse(metering.time);
@@ -52,23 +57,23 @@ angular.module('loggrioApp')
         data.default.push([date, value]);
 
         // sum values of the same day in the last week
-        var dayIndex = new Date(date).getDay();
-        if(date >= oneWeekAgo){
-          if(!days[dayIndex]){
-            days[dayIndex] = {
-              count: 0,
-              value: 0
-            };
+        if (date >= oneWeekAgo) {
+          var day = new Date(date).getDay();
+          var length = data.averageWeek.categories.length;
+
+          // if day is not already in -> create and add first value
+          if (data.averageWeek.categories[length - 1] !== getWeekDay(day)) {
+            counter = 0;
+            data.averageWeek.categories.push(getWeekDay(day));
+            data.averageWeek.values.push(value);
+          // else sum up average
+          } else {
+            counter += 1;
+            var lastValue = data.averageWeek.values[length - 1];
+            data.averageWeek.values[length - 1] = round((lastValue * counter + value) / (counter + 1));
           }
-          days[dayIndex].count++;
-          days[dayIndex].value += value;
         }
       });
-
-      for(var dayIndex in days) {
-        data.averageWeek.values.push(Math.round((days[dayIndex].value / days[dayIndex].count) * 100) / 100);
-        data.averageWeek.categories.push(getWeekDay(dayIndex));
-      }
 
       return data;
     };
